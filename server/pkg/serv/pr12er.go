376146e82e05a51ab2d6cc32e004bc2a06f04a85
Mo Kweon <kkweon@google.com>
2021-05-25 11:04:27 -0700

    Fix serv/pr12er.go wrong embed

--- server/pkg/serv/pr12er.go
+++ server/pkg/serv/pr12er.go
@@ -1,18 +1,21 @@
 package serv
 
 import (
+	"context"
 	"fmt"
-	"log"
-
 	"github.com/codingpot/pr12er/server/pkg/pr12er"
-	"golang.org/x/net/context"
+	"log"
 )
 
 type Server struct {
-	pr12er.Pr12ErServiceServer
+	pr12er.UnimplementedPr12ErServiceServer
 }
 
-func (s *Server) GetHello(ctx context.Context, in *pr12er.HelloRequest) (*pr12er.HelloResponse, error) {
+func (s Server) GetHello(ctx context.Context, in *pr12er.HelloRequest) (*pr12er.HelloResponse, error) {
 	log.Printf("Get Request Hello! body: %s", in.Body)
 	return &pr12er.HelloResponse{Body: fmt.Sprintf("Hello %s", in.Body)}, nil
+}
+
+func (s Server) GetVideos(ctx context.Context, request *pr12er.GetVideosRequest) (*pr12er.GetVideosResponse, error) {
+	panic("implement me")
 }
