a98f6d74a4202b5cb1d45ee98329547bb2e426d2
Mo Kweon <kkweon@google.com>
2021-05-25 20:35:16 -0700

    Add metadata db

---
+++ server/internal/pr12_metadata_test.go
@@ -0,0 +1,17 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/codingpot/pr12er/server/pkg/pr12er"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+func TestEmbededFileCanBeRead(t *testing.T) {
+	var metadataDump pr12er.MetadataDump
+	err := prototext.Unmarshal(PR12MetadataYAML, &metadataDump)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(1), metadataDump.Metadata[0].Id)
+	assert.Equal(t, "Jaejun Yoo", metadataDump.Metadata[0].Presenter)
+}
