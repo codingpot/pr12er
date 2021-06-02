9564c85dcd9cff16598c1d6dd31af4ec54b143d9
hsjeong <nicewook@hotmail.com>
2021-06-02 16:35:15 +0900

    fix: code review

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -31,30 +31,20 @@ var (
 )
 
 var (
-	// Create a metrics registry.
-	reg = prometheus.NewRegistry()
-
-	// Create some standard server metrics.
+	reg         = prometheus.NewRegistry()
 	grpcMetrics = grpc_prometheus.NewServerMetrics()
-
-	// Create a customized counter metric.
-	customizedCounterMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "demo_server_say_hello_method_handle_count",
-		Help: "Total number of RPCs handled on the server.",
-	}, []string{"name"})
-
-	promServer *http.Server
+	promServer  *http.Server
 )
 
 func initPrometheus() {
-	// Register standard server metrics and customized metrics to registry.
-	reg.MustRegister(grpcMetrics, customizedCounterMetric)
-	customizedCounterMetric.WithLabelValues("Test")
-	promServer = &http.Server{Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}), Addr: fmt.Sprintf("0.0.0.0:%d", 9092)}
+	reg.MustRegister(grpcMetrics)
+	promServer = &http.Server{
+		Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
+		Addr:    fmt.Sprintf("0.0.0.0:%s", env.PrometheusPort),
+	}
 }
 
 func initGrpcLogrus() {
-
 	logrusLogger = logrus.New()
 	customFunc = func(code codes.Code) logrus.Level {
 		if code == codes.OK {
@@ -120,7 +110,7 @@ func main() {
 	// Start your http server for prometheus.
 	go func() {
 		if err := promServer.ListenAndServe(); err != nil {
-			log.Fatal("Unable to start a http server.")
+			log.Fatal("Unable to start a prometheus server.")
 		}
 	}()
 
