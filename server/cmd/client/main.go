516d334a71913167b4073113a7a63af7ee00afec
JEONG HYUN SEOK <nicewook@hotmail.com>
2021-06-07 12:51:52 +0900

    feat: serving GPRC GetVideos using dumped pdtxt (#55)
    * feat: serving GPRC GetVideos using dumped pdtxt
    
    - client/main.go can test this GRPC function and save response as .pbtxt
    
    * Update server/cmd/client/main.go
    
    * Update server/pkg/serv/serv.go
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>
    
    * Update server/cmd/client/main.go
    
    * Update server/cmd/client/main.go
    
    * Update server/pkg/serv/serv.go
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>
    
    * Update server/pkg/serv/serv.go
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>
    
    * Update server/pkg/serv/serv.go
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>
    
    * Update serv.go
    
    * Update server/pkg/serv/serv.go
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>
    
    * lint
    
    Co-authored-by: Mo Kweon <kkweon@gmail.com>

--- server/cmd/client/main.go
+++ server/cmd/client/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/codingpot/pr12er/server/pkg/pr12er"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/prototext"
 )
 
 func main() {
+	grpcReq := flag.String("grpcCmd", "", "Test specified GRPC request only, or test all the GRPC requests")
+	flag.Parse()
+
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
 	if err != nil {
-		log.WithError(err).Fatalf("fail to connect")
+		log.WithError(err).Panic("fail to connect")
 	}
 	defer conn.Close()
-
 	c := pr12er.NewPr12ErServiceClient(conn)
 
-	response, err := c.GetHello(context.Background(), &pr12er.HelloRequest{Body: "hi server!"})
-	if err != nil {
-		log.WithError(err).Panic("Error when calling SayHello")
+	if *grpcReq != "video" {
+		resp, err := c.GetHello(context.Background(), &pr12er.HelloRequest{Body: "hi server!"})
+		if err != nil {
+			log.WithError(err).Panic("Error when calling SayHello")
+		}
+		log.Printf("Response from server: %s", resp.Body)
 	}
-	log.Printf("Response from server: %s", response.Body)
+
+	if *grpcReq != "hello" {
+		resp, err := c.GetVideos(context.Background(), &pr12er.GetVideosRequest{})
+		if err != nil {
+			log.WithError(err).Fatal("Error when calling SayHello")
+		}
+		log.Printf("Response from server. Total %d Video(s)", len(resp.Videos))
+
+		respVideosStr := prototext.Format(resp.ProtoReflect().Interface())
+		if err := os.WriteFile("pr12_getvideos.pbtxt", []byte(respVideosStr), 0600); err != nil {
+			log.WithError(err).Fatal("Error on writing file pr12_getvideos.pbtxt")
+		}
+	}
 }
