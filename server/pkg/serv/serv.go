6874080f4a6d4f1c4389920c885176dd2a71e0dc
Mo Kweon <kkweon@gmail.com>
2021-06-09 10:34:27 -0700

    Update server/pkg/serv/serv.go

--- server/pkg/serv/serv.go
+++ server/pkg/serv/serv.go
@@ -22,6 +22,7 @@ func (s Server) GetDetails(_ context.Context, in *pr12er.GetDetailsRequest) (*pr
 			Paper: []*pr12er.Paper{
 				{
 					PaperId: "1",
+					// nolint: lll 
 					Absract: "We propose a new framework for estimating generative models via an adversarial process, in which we simultaneously train two models: a generative model G that captures the data distribution, and a discriminative model D that estimates the probability that a sample came from the training data rather than G. The training procedure for G is to maximize the probability of D making a mistake. This framework corresponds to a minimax two-player game. In the space of arbitrary functions G and D, a unique solution exists, with G recovering the training data distribution and D equal to 1/2 everywhere. In the case where G and D are defined by multilayer perceptrons, the entire system can be trained with backpropagation. There is no need for any Markov chains or unrolled approximate inference networks during either training or generation of samples. Experiments demonstrate the potential of the framework through qualitative and quantitative evaluation of the generated samples.",
 					Repositories: []*pr12er.Repository{
 						{
