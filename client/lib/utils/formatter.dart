9110e31019ebf90d671c1beb748ea5fb35a602d4
chansung park <deep.diver.csp@gmail.com>
2021-06-09 14:37:19 +0900

    test number reformatter

--- client/lib/utils/formatter.dart
+++ client/lib/utils/formatter.dart
@@ -1,11 +1,11 @@
 String reformatNumbers(int number) {
-  int copyNumber = number;
+  double copyNumber = number.toDouble();
   int magnitude = 0;
 
-  while (number.abs() >= 1000) {
+  while (copyNumber.abs() >= 1000) {
     magnitude += 1;
     // ignore: parameter_assignments
-    copyNumber ~/= 1000;
+    copyNumber /= 1000;
   }
 
   return copyNumber.toString() + ['', 'K', 'M', 'G', 'T', 'P'][magnitude];
--- client/test/utils/formatter_test.dart
+++ client/test/utils/formatter_test.dart
@@ -2,10 +2,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pr12er/utils/formatter.dart';
 
 void main() {
-  group("formatNumbers", () {
-    test('format 1000 to 1K', () {
-      int number = 1000;
-      expect(reformatNumbers(number), "1K");
+  group("format numbers", () {
+    test('format 1,000 to 1K', () {
+      expect(reformatNumbers(1000), "1.0K");
+    });
+    test('format 10,000 to 10K', () {
+      expect(reformatNumbers(10000), "10.0K");
+    });
+    test('format 100,000 to 100K', () {
+      expect(reformatNumbers(100000), "100.0K");
+    });
+    test('format 1,000,000 to 1M', () {
+      expect(reformatNumbers(1000000), "1.0M");
+    });
+    test('format 1,100 to 1.1K', () {
+      expect(reformatNumbers(1100), "1.1K");
     });
   });
 }
