8c3f98cab021cdf24396f35f5efaef41e0d3fb73
Mo Kweon <kkweon@google.com>
2021-06-09 22:57:07 -0700

    chore: fix formatting dart

--- client/lib/grpc_channel_web.dart
+++ client/lib/grpc_channel_web.dart
@@ -2,5 +2,6 @@ import 'package:grpc/grpc_web.dart';
 
 GrpcWebClientChannel getKkweonOktetoChannel() {
   // return GrpcWebClientChannel.xhr(Uri.parse("https://envoy-kkweon.cloud.okteto.net"));
-  return GrpcWebClientChannel.xhr(Uri.parse("https://envoy-kkweon.cloud.okteto.net/"));
+  return GrpcWebClientChannel.xhr(
+      Uri.parse("https://envoy-kkweon.cloud.okteto.net/"));
 }
