2635a723dc4ce618dcd72071302af2f728283bec
Mo Kweon <kkweon@google.com>
2021-06-21 22:12:57 -0700

    fix: use ListTile to align in the PopupMenuButton

--- client/lib/screens/main_screen.dart
+++ client/lib/screens/main_screen.dart
@@ -12,57 +12,63 @@ const appName = 'PR12er';
 
 enum VertMenu { themeMode, issueReport }
 
-// ignore: must_be_immutable
 class MainScreen extends StatelessWidget {
   static const String routeName = "main_screen";
-  VideoSearchDelegate videoSearchDelegate = VideoSearchDelegate();
-
+  final VideoSearchDelegate videoSearchDelegate = VideoSearchDelegate();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {
-                // do something
-                showSearch(context: context, delegate: videoSearchDelegate);
-              },
-            ),
-            const SizedBox(width: 5,),
-            PopupMenuButton(
-                itemBuilder: (BuildContext context) => <PopupMenuEntry<VertMenu>>[
-                  CheckedPopupMenuItem<VertMenu>(
-                    value: VertMenu.themeMode,
-                    checked: context.read<CustomTheme>().isDarkMode,
-                    child: const Text('다크모드'),
-                  ),
-                  const PopupMenuDivider(
-                    height: 10,
-                  ),
-                  const PopupMenuItem<VertMenu>(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // do something
+              showSearch(context: context, delegate: videoSearchDelegate);
+            },
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<VertMenu>>[
+              CheckedPopupMenuItem<VertMenu>(
+                value: VertMenu.themeMode,
+                checked: context.read<CustomTheme>().isDarkMode,
+                child: const Text('다크모드'),
+              ),
+              const PopupMenuDivider(height: 5),
+              const PopupMenuItem<VertMenu>(
                   value: VertMenu.issueReport,
-                  child: Text('이슈 리포트'),
-                  ),
-                ],
-              onSelected: (value) {
-                  switch (value) {
-                    case VertMenu.themeMode:
-                      context.read<CustomTheme>().toggleMode();
-                      break;
-                    case VertMenu.issueReport:
-                      break;
-                  }
-              },
-              child: const Icon(Icons.more_vert),
-            ),
-            const SizedBox(width: 15,)
-            // IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
-          ],
-          title: const Text(appName),
-        ),
-        body: PRVideos(videoSearchDelegate: videoSearchDelegate),);
+                  child: ListTile(
+                      // 투명 아이콘을 설정해서 다크모드 와 텍스트 Alignment 유지
+                      leading: Icon(
+                        Icons.report,
+                        color: Colors.transparent,
+                      ),
+                      title: Text("이슈 리포트"))),
+            ],
+            onSelected: (value) {
+              switch (value) {
+                case VertMenu.themeMode:
+                  context.read<CustomTheme>().toggleMode();
+                  break;
+                case VertMenu.issueReport:
+                  break;
+              }
+            },
+            child: const Icon(Icons.more_vert),
+          ),
+          const SizedBox(
+            width: 15,
+          )
+          // IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+        title: const Text(appName),
+      ),
+      body: PRVideos(videoSearchDelegate: videoSearchDelegate),
+    );
   }
 }
 
