d2e04099a0439c60802bf28d1b44c8733a22d621
Mo Kweon <kkweon@google.com>
2021-06-29 15:18:14 -0700

    feat: add GitHub handler and dependency injection

--- server/pkg/env/env.go
+++ server/pkg/env/env.go
@@ -10,6 +10,8 @@ var (
 	ServicePort = getEnvVar("PR12ER_GRPC_PORT", "9000")
 	// PrometheusPort is the port for Prometheus metrics (/metrics).
 	PrometheusPort = getEnvVar("PR12ER_PROMETHEUS_PORT", "9092")
+	// GitHubAPIKey is the GitHub API Key used to create an issue.
+	GitHubAPIKey = getEnvVar("GITHUB_API_KEY", "")
 )
 
 func getEnvVar(key, fallbackValue string) string {
--- server/pkg/serv/serv.go
+++ server/pkg/serv/serv.go
@@ -11,12 +11,19 @@ import (
 
 type Server struct {
 	pr12er.UnsafePr12ErServiceServer
+	h *handlers.Handler
+}
+
+func New(handler *handlers.Handler) *Server {
+	return &Server{
+		h: handler,
+	}
 }
 
 var _ pr12er.Pr12ErServiceServer = (*Server)(nil)
 
 func (s Server) GetDetail(_ context.Context, in *pr12er.GetDetailRequest) (*pr12er.GetDetailResponse, error) {
-	return handlers.DetailResponseFromDB(in.GetPrId(), &data.DB)
+	return s.h.DetailResponseFromDB(in.GetPrId(), &data.DB)
 }
 
 func (s Server) GetHello(_ context.Context, in *pr12er.HelloRequest) (*pr12er.HelloResponse, error) {
@@ -24,9 +31,9 @@ func (s Server) GetHello(_ context.Context, in *pr12er.HelloRequest) (*pr12er.He
 }
 
 func (s Server) GetVideos(_ context.Context, _ *pr12er.GetVideosRequest) (*pr12er.GetVideosResponse, error) {
-	return handlers.VideosResponseFromDB(&data.DB), nil
+	return s.h.VideosResponseFromDB(&data.DB), nil
 }
 
 func (s Server) Report(_ context.Context, in *pr12er.ReportRequest) (*pr12er.ReportResponse, error) {
-	panic("implement me")
+	return s.h.Report(in)
 }
