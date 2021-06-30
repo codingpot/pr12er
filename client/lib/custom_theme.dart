d2b712f126bcd8171c26a383684eeb4a5439db9a
Mo Kweon <kkweon@google.com>
2021-06-30 13:53:31 -0700

    feat: store isDarkMode to SharedPreference
    such that it's persistent even when the app is re-opened from closed.

--- client/lib/custom_theme.dart
+++ client/lib/custom_theme.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// _kHeadline1 는 Abstract/Recommendations/Repositories 등을 나타내는 헤딩1.
 final _kHeadline1 = GoogleFonts.permanentMarker(fontSize: 25);
+const _sharedPreferenceDarkModeKey = "isDarkMode";
 
 /// CustomTheme 은 전체 테마를 정의합니다.
 ///
@@ -35,8 +38,16 @@ class CustomTheme extends ChangeNotifier {
     return _isDarkMode;
   }
 
+  /// sharedPreferences is only visible for testing.
+  /// It is used to test isDarkMode is being loaded from SharedPreferences.
+  @visibleForTesting
+  Future<SharedPreferences> get sharedPreferences =>
+      SharedPreferences.getInstance();
+
   void toggleMode() {
     _isDarkMode = !_isDarkMode;
+    SharedPreferences.getInstance().then(
+        (pref) => pref.setBool(_sharedPreferenceDarkModeKey, _isDarkMode));
     notifyListeners();
   }
 
@@ -48,4 +59,14 @@ class CustomTheme extends ChangeNotifier {
 
   // This type should not be a widget(e.g. Text) because of mockito support...
   String get text => _isDarkMode ? "다크 모드" : "라이트 모드";
+
+  CustomTheme() {
+    SharedPreferences.getInstance().then((pref) {
+      final isDarkMode = pref.getBool(_sharedPreferenceDarkModeKey);
+      if (isDarkMode != null) {
+        _isDarkMode = isDarkMode;
+        notifyListeners();
+      }
+    });
+  }
 }
