7eb832597033f054fbb2abababacd39e4d7e37c3
kkweon <kkweon@users.noreply.github.com>
2021-07-23 00:26:16 +0000

    Deploying to gh-pages from @ codingpot/pr12er@633a70750072235e3a28dfab82c50afe2f910c25 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "8a6ea62746af22b58abd8a5077b3f8db",
 "main.dart.js": "51fa7293ca7ac9004adec0db5bf9142c",
-"index.html": "7694db19960f846ba1354d3e20b46c87",
-"/": "7694db19960f846ba1354d3e20b46c87",
+"index.html": "4e56ca396a37720f674b4332cbb87e92",
+"/": "4e56ca396a37720f674b4332cbb87e92",
 "manifest.json": "b97f24e9e0e52ea213ec40bde19e5496",
 "assets/images/tf-logo.png": "c14d03d92129a49032b2438eced1d882",
 "assets/images/pytorch-logo.png": "62989affe3e57077963ec5ac0e902389",
