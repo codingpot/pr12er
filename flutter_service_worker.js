b533cf9b3a1464304a06a990ec2cdb0368a9a0a3
kkweon <kkweon@users.noreply.github.com>
2021-08-31 12:17:06 +0000

    Deploying to gh-pages from @ codingpot/pr12er@b63e00eb0434d1ab2b3f642146989800eb9b3642 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "manifest.json": "b97f24e9e0e52ea213ec40bde19e5496",
 "version.json": "f4c169f8a1cc8964c3556bed4c699027",
 "main.dart.js": "51fa7293ca7ac9004adec0db5bf9142c",
-"index.html": "7a236dd8f625599399ff5efadcee3a1c",
-"/": "7a236dd8f625599399ff5efadcee3a1c"
+"index.html": "dda58009ae5d60847ce0c1cd6981ec53",
+"/": "dda58009ae5d60847ce0c1cd6981ec53"
 };
 
 // The application shell files that are downloaded before a service worker can
