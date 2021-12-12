0059bc71ef77e9f352291f59ba5bf8e0704ba1f3
Mo Kweon <kkweon@gmail.com>
2021-12-11 21:37:20 -0800

    fix: flutter build lint errors (#176)

--- client/lib/custom_theme.dart
+++ client/lib/custom_theme.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
