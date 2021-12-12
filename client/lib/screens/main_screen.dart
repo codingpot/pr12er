378f5fbf45e2eee1fa88524ebd3a3c7231469523
Mo Kweon <kkweon@google.com>
2021-12-11 21:06:49 -0800

    fix: flutter build lint errors

--- client/lib/screens/main_screen.dart
+++ client/lib/screens/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
 import '../protos/pkg/pr12er/messages.pb.dart';
--- client/lib/widgets/detail/youtube_native.dart
+++ client/lib/widgets/detail/youtube_native.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
